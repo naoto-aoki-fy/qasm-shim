8272fda8ee6a5ec904e2d95c9989bc606dbdc575
Naoto Aoki <184037301+naoto-aoki-fy@users.noreply.github.com>
2025-07-26 20:34:35 +0900

    allow qubits indexing with initializer_list

--- userqasm.cpp
+++ userqasm.cpp
@@ -8,7 +8,7 @@ public:
 
         qubits q1 = qalloc(8);
         qubits q2 = qalloc(8);
-        (negctrl<2>() * ctrl<2>() * h())(q1[0], q1[slice(1, 2)], q1[set{3, 4}]);
+        (negctrl<2>() * ctrl<2>() * h())(q1[0], q1[slice(1, 2)], q1[{3, 4}]);
         u(0, 0, 1.0)(q1[0]);
         u(0, 0, 0.5)(q1[0]);
         (ctrl<2>() * pow(0.5) * u(0, 0, 1.0))(q1[0], q1[1], q1[2]);
