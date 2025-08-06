209889fd279a023c78f35e58e8f43c37befce092
Naoto Aoki <184037301+naoto-aoki-fy@users.noreply.github.com>
2025-08-06 19:11:07 +0900

    update userqasm.cpp

--- userqasm.cpp
+++ userqasm.cpp
@@ -3,17 +3,24 @@
 class userqasm : public qasm::qasm
 {
 public:
+    bit clbit;
     void circuit() {
         using namespace qasm;
 
         qubits q1 = qalloc(8);
         qubits q2 = qalloc(8);
+        clbit = clalloc(16);
+        reset(q1);
+        reset(q2);
         (negctrl(2) * ctrl(2) * h())(q1[0], q1[slice(1, 2)], q1[{3, 4}]);
-        u(0, 0, 1.0)(q1[0]);
-        u(0, 0, 0.5)(q1[0]);
-        (ctrl(2) * pow(0.5) * u(0, 0, 1.0))(q1[0], q1[1], q1[2]);
+        cu(0, 0, 1.0, 0)(q1[0], q1[1]);
+        cu(0, 0, 0.5, 0)(q1[0], q1[1]);
+        (ctrl(2) * pow(0.5) * cu(0, 0, 1.0, 0))(q1[0], q1[1], q1[2], q1[3]);
         (inv() * h())(q1[0]);
         (ctrl(2) * h())(q2[0], q2[1], q2[2]);
+
+        clbit[slice(0, 7)] = measure(q1[slice(0, 8)]);
+        clbit[{8, 9}] = measure(q2[{0, 1}]);
 
     }
 };
